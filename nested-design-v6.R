dcd11070d8f3c09ca2507135cc2780423ad1091f
Brian W Bush <brian.bush@nrel.gov>
2020-04-11 20:37:04 -0600

    nexted designs

---
+++ nested-design-v6.R
@@ -0,0 +1,354 @@
+
+require(abind)
+require(data.table)
+require(deSolve)
+require(magrittr)
+require(rTensor)
+
+require(ggplot2)
+require(GGally)
+
+
+# Create a multivariate function with specified properties:
+#   tmax: maximum time
+#   multiplicities: number of correlations each parameter has
+#   degrees: polynomial degree of each parameter
+#   dimension: the dimension of the output
+#   returns a multivariate function of the vector of parameters and time
+makeGenerator <- function(multiplicities, degrees, dimension) {
+
+    single <- function(degree) {
+      x0 <- runif(1)
+      z0 <- runif(1)
+      function(x) {
+          if (x < x0)
+              0
+          else
+              z0 * (x - x0)^degree
+      }
+    }
+  
+    locations <- lapply(multiplicities, function(m) sample(1:dimension, m))
+    functions <- lapply(degrees, single)
+    
+    start <- runif(dimension, -0.25, 0.75)
+    coefs <- matrix(runif(dimension^2, -0.25, 0.75), dimension, dimension)    
+    shift <- matrix(runif(dimension^2, -0.25, 0.75), dimension, dimension)
+    
+    function(x, ts) {
+        z <- rep(0, dimension)
+        for (i in 1:length(locations))
+            for (j in locations[[i]])
+                z[j] <- z[j] + functions[[i]](x[i])
+        ode(start, ts, function(t, y, params) {list((coefs %*% y) * z * (1 - ((shift %*% y) * z)))})
+    }
+    
+}
+
+
+ssa.digits <- 30
+
+ssa.scale <- 2^ssa.digits
+
+ssa.level <- function(i) {
+    if (i == 0)
+        return(ssa.digits)
+    n <- 0
+    while (bitwAnd(i, 1) == 0) {
+        n <- n + 1
+        i <- bitwShiftR(i, 1)
+    }
+    n
+}
+
+ssa.depth <- function(i) {
+    ssa.digits - ssa.level(i)
+}
+
+ssa.corner <- function(i, offset=0){
+    d <- ssa.level(i) - offset
+    c(i - 2^d, i + 2^d)
+}
+
+ssa.corners <- function(i1, i2, i3, offset=0) {
+    axis <- function(s1, s2, s3) {
+        if (s1 == 0)
+            4 * s1 + 2 * s2 + s3
+        else
+            4 * (1 - s1) + 2 * (1 - s2) + (1 - s3)
+    }
+    merge(merge(merge(
+        data.table(
+            d1 = 1,
+            d2 = 2,
+            d3 = 4
+        ),
+        data.table(d1=1, i1=ssa.corner(i1, offset=offset), s1=0:1), by="d1", allow.cartesian=TRUE),
+        data.table(d2=2, i2=ssa.corner(i2, offset=offset), s2=0:1), by="d2", allow.cartesian=TRUE),
+        data.table(d3=4, i3=ssa.corner(i3, offset=offset), s3=0:1), by="d3", allow.cartesian=TRUE
+    )[,
+    .(
+        i1,
+        i2,
+        i3,
+        axis=mapply(axis, s1, s2, s3)
+    )]
+}
+
+ssa.candidates <- function(i1, i2, i3) {
+    CJ(
+        i1=c(i1, ssa.corner(i1)),
+        i2=c(i2, ssa.corner(i2)),
+        i3=c(i3, ssa.corner(i3))
+    )[,
+        .(
+            i1,
+            i2,
+            i3,
+            x1 = i1 / ssa.scale,
+            x2 = i2 / ssa.scale,
+            x3 = i3 / ssa.scale
+        )
+    ]
+}
+
+ssa.start <- function() {
+    result <- CJ(
+        i1=c(0, ssa.scale),
+        i2=c(0, ssa.scale),
+        i3=c(0, ssa.scale)
+    )[,
+        .(
+            sequence = 1:8     ,
+            generation = 0     ,
+            i1                 ,
+            i2                 ,
+            i3                 ,
+            x1 = i1 / ssa.scale,
+            x2 = i2 / ssa.scale,
+            x3 = i3 / ssa.scale,
+            compute = TRUE     ,
+            measure = FALSE    ,
+            probed  = TRUE     ,
+            s1 = 0             ,
+            s2 = 0             ,
+            s3 = 0             ,
+            s  = 0
+        )
+    ]
+    result <- rbind(
+        result,
+        data.table(
+            sequence = 9      ,
+            generation = NA   ,
+            i1 = ssa.scale / 2,
+            i2 = ssa.scale / 2,
+            i3 = ssa.scale / 2,
+            x1 = 0.5          ,
+            x2 = 0.5          ,
+            x3 = 0.5          ,
+            compute = FALSE   ,
+            measure = TRUE    ,
+            probed  = FALSE   ,
+            s1 = 0. / 0.      ,
+            s2 = 0. / 0.      ,
+            s3 = 0. / 0.      ,
+            s  = 0. / 0.
+        )
+    )
+    setkeyv(result, c("i1", "i2", "i3"))
+}
+
+ssa.compute <- function(f, ts, xs, ys) {
+    needed <- xs[compute == TRUE]
+    for (row in 1:nrow(xs)) {
+        ys <- rbind(
+            ys,
+            data.table(f(as.numeric(xs[row, .(x1, x2, x3)]), ts))[,
+                .(
+                    sequence = xs[row, sequence],
+                    t = time,
+                    y1 = `1`,
+                    y2 = `2`,
+                    y3 = `3`
+                )
+            ]
+        )
+    }
+    xs[compute == TRUE, compute := FALSE]
+    ys
+}
+
+ssa.measure <- function(xs, ys, focus=2) {
+    
+    result <- merge(
+        merge(
+            xs[measure == TRUE][, ssa.corners(i1, i2, i3), by=.(center=sequence)],
+            xs,
+            by=c("i1", "i2", "i3"),
+            allow.cartesian=TRUE
+        )[, .(center, axis, sequence)],
+        ys,
+        by="sequence",
+        allow.cartesian=TRUE
+    )[,
+        .(y1 = mean(y1), y2 = mean(y2), y3 = mean(y3)), by=.(sequence=center, axis, t)
+    ][,
+        .(s1 = sd(y1), s2 = sd(y2), s3 = sd(y3)), by=.(sequence, t)
+    ][,
+        .(s1 = max(s1), s2 = max(s2), s3 = max(s3)), by=sequence
+    ]
+
+    xs[
+        sequence %in% result$sequence,
+        `:=`(
+            s1=result$s1,
+            s2=result$s2,
+            s3=result$s3,
+            generation=xs[, 1 + max(generation, na.rm=TRUE)]
+        )
+    ]
+    
+    y1min = min(ys$y1)
+    y1max = max(ys$y1)
+    y2min = min(ys$y2)
+    y2max = max(ys$y2)
+    y3min = min(ys$y3)
+    y3max = max(ys$y3)
+
+    normalize <- function (i, s1, s2, s3)
+        max(s1 / (y1max - y1min), s2 / (y2max - y2min), s3 / (y3max - y3min)) * focus^ssa.depth(i)
+    xs[, `:=`(measure = FALSE, s = mapply(normalize, i1, s1, s2, s3))]
+    
+    result
+}
+
+ssa.probe <- function(xs) {
+    probe <- xs[
+        probed == FALSE,
+        .(sequence, i1, i2, i3, rank=frank(-s, ties.method="random"))
+    ][
+        rank == 1,
+        .(sequence, i1, i2, i3)
+    ]
+    candidates <- ssa.candidates(probe$i1, probe$i2, probe$i3)
+    setkeyv(candidates, c("i1", "i2", "i3"))
+    candidates <- candidates[!xs]
+    candidates <- candidates[, .(
+        sequence = xs[, max(sequence)] + (1:nrow(candidates)),
+        generation = NA,
+        i1, i2, i3,
+        x1, x2, x3,
+        compute = TRUE ,
+        measure = FALSE,
+        probed  = FALSE,
+        s1 = 0. / 0.,
+        s2 = 0. / 0.,
+        s3 = 0. / 0.,
+        s  = 0. / 0.
+    )]
+    probes <- ssa.corners(probe$i1, probe$i2, probe$i3, offset=1)
+    probes <- probes[, .(
+        sequence = max(candidates$sequence) + (1:nrow(probes)),
+        generation = NA,
+        i1, i2, i3,
+        x1 = i1 / ssa.scale,
+        x2 = i2 / ssa.scale,
+        x3 = i3 / ssa.scale,
+        compute = FALSE,
+        measure = TRUE ,
+        probed  = FALSE,
+        s1 = 0. / 0.,
+        s2 = 0. / 0.,
+        s3 = 0. / 0.,
+        s  = 0. / 0.
+    )]
+    result <- rbind(
+        xs,
+        candidates,
+        probes
+    )
+    result[sequence == probe$sequence, `:=`(compute=TRUE, measure=FALSE, probed=TRUE)]
+    setkeyv(result, c("i1", "i2", "i3"))
+    list(
+        sequence=probe$sequence,
+        xs=result
+    )
+}
+
+RNGkind("Mersenne-Twister", "Inversion", "Rejection")
+
+set.seed(46)
+
+f <- makeGenerator(c(2, 2, 3), c(0, 1, 2), 3)
+
+ts <- (0:20) / 2
+
+xs <- ssa.start()
+
+xs
+
+ys <- NULL
+sequences <- NULL
+
+for (k in 1:250) {
+    # Perform pending computations.
+    ys <- ssa.compute(f, ts, xs, NULL)
+    # Measure divergences.
+    ssa.measure(xs, ys, focus = (sqrt(5) + 1) / 2)
+    # Select where to probe further.
+    result <- ssa.probe(xs)
+    # Tally results.
+    sequences <- rbind(
+        sequences,
+        xs[
+            sequence == result$sequence,
+            .(
+                sequence,
+                x1, x2, x3,
+                generation,
+                depth=mapply(ssa.depth, i1))
+        ]
+    )
+    # Update grid.
+    xs <- result$xs
+}
+sequences
+
+write.table(xs, file="xs-v6.csv", row.names=FALSE, sep=",")
+xs %>% dim
+
+write.table(ys, file="ys-v6.csv", row.names=FALSE, sep=",")
+ys %>% dim
+
+ggplot(
+    xs[!is.na(generation), .(Generation=factor(generation), Discrepancy=s, Depth=factor(mapply(ssa.depth, i1)))],
+    aes(x=Discrepancy, fill=Depth)
+) + geom_histogram(bins = 30)
+
+ggplot(
+    xs[!is.na(generation), .(Generation=factor(generation), Discrepancy=s, Depth=factor(mapply(ssa.depth, i1)))],
+    aes(x=Discrepancy, fill=Generation)
+) + geom_histogram(bins = 30)  + theme(legend.position = "none")
+
+ggplot(
+    xs[!is.na(generation), .(Generation=generation, Discrepancy=s, Depth=factor(mapply(ssa.depth, i1)))],
+    aes(x=Generation, y=Discrepancy, color=Depth)
+) + geom_point()
+
+ggplot(
+    xs[compute == FALSE, .(N=.N, Depth=max(mapply(ssa.depth, i1))), by=.(x1, x2)],
+    aes(x=x1, y=x2, size=N, color=Depth)
+) + geom_point()
+
+ggplot(
+    xs[compute == FALSE, .(N=.N, Depth=max(mapply(ssa.depth, i1))), by=.(x2, x3)],
+    aes(x=x2, y=x3, size=N, color=Depth)
+) + geom_point()
+
+ggplot(
+    xs[compute == FALSE, .(N=.N, Depth=max(mapply(ssa.depth, i1))), by=.(x3, x1)],
+    aes(x=x3, y=x1, size=N, color=Depth)
+) + geom_point()
+
+ggpairs(xs, 6:8)
